969f83262e702aa0ab3a2467dfdf713d5fe8d033
Suresh Attanayake <suresh.attanayake@gmail.com>
2024-10-07 00:22:21 +0200

    basic math

---
+++ basic_math.py
@@ -0,0 +1,25 @@
+x = 10
+y = 2
+
+z = x + y
+z2 = x/y
+
+print(z)
+print(type(z)) # still an int
+
+print(z2)
+print(type(z2)) # now a float
+
+# base raised to the power
+n = 2**3
+print(n)
+
+# mod
+m = 11 % 3
+print(m)
+
+# python follow BODMAS
+a = 7+10/2*3-1
+b = (7+((10/2)*3))-1
+print(a)
+print(b)
